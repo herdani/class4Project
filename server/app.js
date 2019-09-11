4501c7cb4c29cbe6bb1343cd9b05d0ac7968d405
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-09-11 11:13:20 +0200

    Week1 Assignment

---
+++ server/app.js
@@ -0,0 +1,19 @@
+// Purpose of file: To initialize Express and create basic endpoints
+const express = require("express"); // Loading in Express functionality
+const apiRouter = require("./api"); // Loading in our custom index.js from /api (it will automatically look for index.js)
+const app = express(); // Creating an Express instance
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("*", function logGetRequests(req, res, next) {
+  console.log("someone made a request with GET method");
+  next();
+});
+
+app.get("/api", apiRouter);
+
+app.get("/", function(req, res) {
+  res.send("index page, triggered by GET /");
+});
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,30 +1,7 @@
-const express = require("express");
-
-const app = express();
-
-const apiRouter = express.Router();
-
-app.use("*", function logGetRequests(req, res, next) {
-  console.log("Someone made a request with the GET method");
-  next();
-});
-
-apiRouter.get("/", function(req, res) {
-  res.send("triggered by GET /api/");
-});
-
-apiRouter.post("/add", function(req, res) {
-  res.send("triggered by POST /api/add");
-});
-
-app.use("/api", apiRouter);
-
-app.get("/", function(req, res) {
-  res.send("index page, triggered by GET /");
-});
+// Purpose of file: To start our HTTP server
+const app = require("./app"); // This loads in the code from app.js
 
 const PORT = 8080;
-
 app.listen(PORT, function() {
-  console.log(`The server is running on port ${PORT}`);
+  console.log(`The server is is running on port ${PORT}`);
 });
