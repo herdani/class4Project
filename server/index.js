7127f3483cce22b80ddb55d78ca8ddc1f7aace86
sena <48647939+msenaavci@users.noreply.github.com>
2019-09-13 15:20:13 +0200

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,8 @@
+
+
+const app = require("./app"); // This loads in the code from app.js
+
+const PORT = 8080;
+app.listen(PORT, function() {
+  console.log(`The server is is running on port ${PORT}`);
+});
