00ddceac2af76fd421434401ae99029a87d42681
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-09-14 23:23:05 +0200

    edited App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>This is Alfi's final project page</h1>
+      <header>
+        <h1>Messages From Street</h1>
+      </header>
+      <form>
+        <input type="text"></input>
+      </form>
     </div>
   );
 }
