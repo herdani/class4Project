0e37946a7815605f966a5806c5e30afb2dde32f8
FaziletKosure <47929186+FaziletKosure@users.noreply.github.com>
2019-09-16 22:04:43 +0200

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1984Fkek',
+    password: '1234',
     database: 'class4project',
 });
 connection.connect(err => {
