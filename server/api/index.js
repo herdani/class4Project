7504dc3d477d9e1023b3998b8763ecd726d5602f
FaziletKosure <faziletkosure1@gmail.com>
2019-09-18 20:16:23 +0200

    DatabaseUpdated

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,14 @@
 const apiRouter = require('express').Router();
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const password = require('../../config');
+// const password = require('../../config');
 
-const connection = mysql.createConnection(password);
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1984Fkek',
+    database: 'class4project',
+});
 connection.connect(err => {
     if (!err) console.log('DB connection succeded.');
     else
@@ -23,6 +28,30 @@ apiRouter.get('/', function(req, res) {
     res.send('triggered by GET /api/ path');
 });
 
+// Get all messages
+const SELECT_ALL_MESSAGES_QUERY = 'SELECT * FROM messages';
+apiRouter.get('/messages', (req, res) => {
+    connection.query(SELECT_ALL_MESSAGES_QUERY, (err, results) => {
+        if (err) {
+            return res.send(err);
+        }
+        return res.json({
+            messages: results,
+        });
+    });
+});
+// Get an message
+apiRouter.get('/message/:id', (req, res) => {
+    connection.query(
+        'SELECT * FROM messages WHERE id = ?',
+        [req.params.id],
+        (err, rows, fields) => {
+            if (!err) res.send(rows);
+            else console.log(err);
+        }
+    );
+});
+
 apiRouter.post('/message/add', function(req, res) {
     const message = req.body.body;
     console.log(req.body);
@@ -36,6 +65,17 @@ apiRouter.post('/message/add', function(req, res) {
         }
     );
     res.send(req.body);
+});
+// Delete an message
+apiRouter.delete('/message/:id', (req, res) => {
+    connection.query(
+        'DELETE FROM messages WHERE id = ?',
+        [req.params.id],
+        (err, rows, fields) => {
+            if (!err) res.send('Deleted successfully.');
+            else console.log(err);
+        }
+    );
 });
 
 // Application initialization
