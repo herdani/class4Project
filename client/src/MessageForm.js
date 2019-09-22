01fa95ae4eec34261a90c6369bfdaab94a8d16cd
erhan <erhan.m.aky@gmail.com>
2019-09-22 23:36:39 +0200

    front-end can send the messages to back-end now. but submit status remains false..

--- client/src/MessageForm.js
+++ client/src/MessageForm.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
+import { Redirect } from 'react-router-dom';
+
 const baseUrl = 'http://localhost:8080';
+
 
 
 class MessageForm extends Component {
@@ -7,48 +10,72 @@ class MessageForm extends Component {
     constructor(props){
         super(props);
         this.state = {
-            body: "",
-            license_plate: ""
-        };
+            submitted: false
+        }
       }
-    addMessage = async (data) => {
-/*         return fetch(
-            "/message/add", {
-                method: "POST",
-                headers: {
-                "content-type": "application/json"
-                },
-                body: JSON.stringify(data)
-            }).then(response => response.json());
-        }; */
+/*     handleLicense = async (event) => {
+        event.preventDefault();
+        console.log('License Added');
+        this.addMessage({
+            body: this.state.body,
+            license_plate: this.state.license_plate
+        });
+    }
 
+    handleMessage = async (event) => {
+        
+    } */
+    addMessage = async (data) => {
         const response  = await fetch(`${baseUrl}/api/message/add`, {
-        method: 'POST',
-        headers: {"content-type": "application/json"},
-        mode: 'cors',
-        body: JSON.stringify(data)
+            method: 'POST',
+            headers: {"content-type": "application/json"},
+            mode: 'cors',
+            body: JSON.stringify(data)
         })
         console.log(response.body);
         return response.json();
     };
 
-    
     handleSubmit = async (event) => {
         event.preventDefault();
         console.log('clicked');
-        this.addMessage({
-            body: this.state.body,
-            license_plate: this.state.license_plate
+
+        const messageData = new FormData(event.target);
+        await this.addMessage({
+            license_plate: messageData.get('license'),
+            body: messageData.get('body')
+        });
+
+        this.setState({
+            submitted: true
         });
 
     };
 
+/*     async componentDidMount() {
+        const message = await this.addMessage();
+        this.setState({
+            message
+        });
+    } */
+    
     render() {
         return(
-            <form onSubmit={this.handleSubmit} >
-            License: <input type="text" name="License" defaultValue={this.state.license_plate}/><br/><br/>
-            Message: <textarea type="text" name="Body" defaultValue={this.state.body}/><br/>
-            <input type="submit" value="Submit"/>
+            <form onSubmit={this.handleSubmit} className="MessageForm">
+                
+                {/* this.state.submitted ? <Redirect to="/"/> : null */}
+                <div>
+                    <label htmlFor="license">Enter License Plate</label>
+                    <input id="license" name="license" type="text" />
+                </div>
+             <br/>
+                <div> 
+                    <label htmlFor="body">Enter Message</label>
+                    <textarea id="body" name="body" type="text"/>
+                </div>
+                <div>
+                    <input type="submit" value="Submit"/>
+                </div>            
             </form>
         )
     }
