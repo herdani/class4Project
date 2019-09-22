1dfb562ce3574038ad494a1e4ca5d4633e02eef9
codelawer <codelawer@gmail.com>
2019-09-22 13:54:49 +0200

    modified --no-verify

--- server/api/index.js
+++ server/api/index.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1234',
+    password: '56789',
     database: 'class4project',
 });
 
