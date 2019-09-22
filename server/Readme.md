901334373e4b424bf19d5a54e825606e8d5f4d32
codelawer <codelawer@gmail.com>
2019-09-22 16:20:11 +0200

    del user story

---
+++ server/Readme.md
@@ -0,0 +1,3 @@
+//
+
+ALTER TABLE `class4project`.`messages` ADD COLUMN `Deleted` TINYINT NULL DEFAULT False AFTER `license_plate`;
--- server/api/index.js
+++ server/api/index.js
@@ -18,17 +18,35 @@ apiRouter.get('/', function(req, res) {
     res.send('triggered by GET /api/ path');
 });
 
+// Post Message
 apiRouter.post('/message/add', (req, res) => {
     const { body } = req.body;
     const { license_plate } = req.body;
     const insertMessage = `INSERT INTO messages (body, submission_date, license_plate) VALUES (?,now(),?);`;
     connection.query(insertMessage, [body, license_plate], (err, result) => {
         if (err) throw err;
-        console.log('post request made: ' + result);
+        console.log(`post request made: ${result}`);
         res.send(result);
     });
 });
 
+// Delete Message
+apiRouter.delete('/message/delete', (req, res) => {
+    const { id } = req.body;
+    console.log(id);
+    const showAll = `select * from messages;`;
+    const changeBoolean = `update messages set deleted = "1" where id = '${id}'`;
+
+    connection.query(showAll, (err, result) => {
+        if (err) {
+            throw err;
+        } else
+            connection.query(changeBoolean, (err, resBool) => {
+                if (err) throw err;
+                res.json(resBool);
+            });
+    });
+});
 // Application initialization
 
 module.exports = apiRouter;
