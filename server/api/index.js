e96ed1af6a09a2d0458ec4004ad7f88246186496
Nihan2019 <fnakkose@hotmail.com>
2019-09-22 15:32:01 +0200

    modified the backend and frontend

--- server/api/index.js
+++ server/api/index.js
@@ -3,10 +3,10 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'class4project',
+  host: 'localhost',
+  user: 'nihan',
+  password: '12345',
+  database: 'class4Project',
 });
 
 connection.connect();
@@ -14,19 +14,19 @@ connection.connect();
 // parse application/json
 apiRouter.use(bodyParser.json());
 
-apiRouter.get('/', function(req, res) {
-    res.send('triggered by GET /api/ path');
+apiRouter.get('/', function (req, res) {
+  res.send('triggered by GET /api/ path');
 });
 
 apiRouter.post('/message/add', (req, res) => {
-    const { body } = req.body;
-    const { license_plate } = req.body;
-    const insertMessage = `INSERT INTO messages (body, submission_date, license_plate) VALUES (?,now(),?);`;
-    connection.query(insertMessage, [body, license_plate], (err, result) => {
-        if (err) throw err;
-        console.log('post request made: ' + result);
-        res.send(result);
-    });
+  const { body } = req.body;
+  const { license_plate } = req.body;
+  const insertMessage = `INSERT INTO messages (body, submission_date, license_plate) VALUES (?,now(),?);`;
+  connection.query(insertMessage, [body, license_plate], (err, result) => {
+    if (err) throw err;
+    console.log('post request made: ' + result);
+    res.send(result);
+  });
 });
 
 // Application initialization
