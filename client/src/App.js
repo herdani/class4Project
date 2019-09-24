60c76f59d70734e2c590b73468d635e0638b3d70
codelawer <codelawer@gmail.com>
2019-09-24 14:40:12 +0200

    sample react-share npm applied

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ReactShare from './components/ReactShare';
 
 function App() {
     return (
         <div className="App">
-            <h1>Hello World</h1>
+            <ReactShare />
         </div>
     );
 }
