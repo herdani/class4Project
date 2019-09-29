9ee2cbcf7e5d2624e55118ad4c8ec9a09c737763
codelawer <codelawer@gmail.com>
2019-09-29 15:32:35 +0200

    delete user story added to server/index.js file

--- server/api/index.js
+++ server/api/index.js
@@ -1,25 +1,20 @@
-
 require('dotenv').config();
 const apiRouter = require('express').Router();
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
-
 // ./loginDB file is created in api folder, and added to gitignore.
 // Enter your own login credentials for your MySql database in that file, so no hard coding will be required after push/pull.
 const connection = mysql.createConnection({
-
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_USERPASS,
     database: process.env.DB_NAME,
-
 });
 connection.connect();
 
 // parse application/json
 apiRouter.use(bodyParser.json());
-
 
 // Used list_existing_messages
 apiRouter.get('/', function(req, res) {
@@ -28,9 +23,9 @@ apiRouter.get('/', function(req, res) {
         if (err) throw err;
         res.json(result);
     });
-
 });
 
+// Post Message
 apiRouter.post('/message/add', (req, res) => {
     const { body } = req.body;
     const { license_plate } = req.body;
@@ -39,7 +34,6 @@ apiRouter.post('/message/add', (req, res) => {
         if (err) throw err;
         console.log(`post request made: ${result}`);
         res.send(result);
-
     });
     //
     const api_key = '637309d8094f9b578d6f7a68cfd5d181-baa55c84-d6fb89fe';
@@ -49,7 +43,6 @@ apiRouter.post('/message/add', (req, res) => {
 
     const data = {
         from:
-
             'class4Project <mailgun@sandboxba59f9aaff77478d9b4c22a8f7ee1ee2.mailgun.org>',
         to: 'avci.msena@gmail.com',
 
@@ -65,6 +58,24 @@ apiRouter.post('/message/add', (req, res) => {
         } else {
             console.log(body);
         }
+    });
+});
+
+// Delete Message
+apiRouter.delete('/message/delete/${id}', (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    const showAll = `select * from messages;`;
+    const changeBoolean = `update messages set deleted = "1" where id = '${id}'`;
+
+    connection.query(showAll, (err, result) => {
+        if (err) {
+            throw err;
+        } else
+            connection.query(changeBoolean, (err, resBool) => {
+                if (err) throw err;
+                res.json(resBool);
+            });
     });
 });
 
