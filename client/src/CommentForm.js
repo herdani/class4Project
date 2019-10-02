e744b281b94faad20bdd75cdc14a4bf183e67e7c
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-10-02 09:36:00 +0200

    added comment box

--- client/src/CommentForm.js
+++ client/src/CommentForm.js
@@ -1,18 +1,32 @@
 import React, {Component} from 'react';
-import api from './apiClient';
 import './CommentForm.css';
+import api from './apiClient';
+import {CommentButton} from './views/CommentButton';
 
 class CommentForm extends Component {
+  constructor(props){
+      super(props);
+      this.state = {
+          commented: false
+      }
+  }
+
+handleClickComment = async(event) => {
+  event.preventDefault();
+    console.log("comment");
+  }
     render() {
         return(
-            <form className="CommentForm">
-                <div>
-                    <label htmlFor="body">Message</label>
-                    <textarea id="body" name="body" type="text"/>
+            <form  className="CommentForm">
+              <div>
+                <label htmlFor="email">Email</label>
+                <input id="email" name="email" type="email" />
+              </div>
+              <div>
+                  <label htmlFor="body">Comment</label>
+                  <textarea id="body" name="body" type="text"/>
                 </div>
-                <div>
-                    <input type="submit" value="Submit"/>
-                </div>
+              <CommentButton onClick = {this.handleClickComment}/>
             </form>
         )
     }
--- client/src/MessageItem.js
+++ client/src/MessageItem.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import moment from 'moment';
 import './MessageItem.css';
-
+import CommentForm from './CommentForm';
 export default ({ _id, submission_date, license_plate, body}) => {
     var timePosted = moment(submission_date).format("DD/MM/YYYY - HH:mm");
+
     return (
       <li className="MessageItem">
         <span>Time: {timePosted} -  </span>
         <span>To license: {license_plate} : </span>
         <span>   {body}  </span>
-        <button> Comment </button>
+        <CommentForm />
       </li>
     )
 }
