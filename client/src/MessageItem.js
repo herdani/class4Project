15c4e2459d273f36777acc46a52131796c4719e3
codelawer <codelawer@gmail.com>
2019-10-13 13:01:37 +0200

    except from e-mail all of them works, status:cancelled

--- client/src/MessageItem.js
+++ client/src/MessageItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import moment from 'moment';
 import './MessageItem.css';
+import ShareMessage from './ShareMessage';
 
 export default ({ id, submission_date, license_plate, body, handleDelete }) => {
     var timePosted = moment(submission_date).format('DD/MM/YYYY - HH:mm');
@@ -11,6 +12,12 @@ export default ({ id, submission_date, license_plate, body, handleDelete }) => {
             <span>To license: {license_plate} : </span>
             <span> {body} </span>
             <button onClick={() => handleDelete(id)}>X</button>
+            <ShareMessage
+                id={id}
+                license_plate={license_plate}
+                body={body}
+                submission_date={submission_date}
+            />
         </li>
     );
 };
